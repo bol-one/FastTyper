de29b2af72a2a0bdd5c1f38638f9f200b415e1fe
bol-one <wnlp404@gmail.com>
2025-06-02 19:30:19 +0300

    Update FastTyper.py

--- FastTyper.py
+++ FastTyper.py
@@ -4,13 +4,13 @@ from sys import stdin
 from time import sleep
 
 ch = int(input('1 Обычный режим\n2 Режим спама\nВведите режим: '))
-key = input('Введите клавишу перед вводом в чат (необ.): ') or ''
+key = input('Введите клавишу перед вводом в чат (необ.): ')
 
 if ch == 1:
     print("Введите текст (Ctrl+Z, а затем enter для завершения)")
     text = stdin.read().split()
     a = float(input('Введите задержку сообщений (в секундах): '))
-    x, y = map(int, input('рандом от x до y (1 10): ').split())
+    x, y = map(int, input('рандом слов от x до y (1 10): ').split())
     print('Нажмите ` для начала, Ctrl + Z в консоли для завершения')
     wait('`')
     while text:
